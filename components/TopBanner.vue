dc9145081db722f65e7e2080612b758adad78a83
raulsposito <raulsposito@gmail.com>
2022-03-18 12:44:28 -0300

    cleanup and wrap up

--- components/TopBanner.vue
+++ components/TopBanner.vue
@@ -5,9 +5,9 @@
     </div>
     <div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
-        <div class="flex">
-            <img class="h-24 w-24 object-cover rounded-full ring-4 ring-white sm:h-32 sm:w-32" src="https://images.unsplash.com/photo-1622867072220-b76505e09a68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Vintage movie roll">
-        </div>
+        <NuxtLink to="/curious" type="button" class="flex animate-spin">
+            <img class="h-24 w-24 object-cover rounded-full ring-4 ring-white sm:h-32 sm:w-32 hover:animate-none" src="https://images.unsplash.com/photo-1622867072220-b76505e09a68?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80" alt="Vintage movie roll">
+        </NuxtLink>
         <div class="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
             <div class="sm:hidden md:block mt-6 min-w-0 flex-1">
             <h1 class="text-3xl font-bold text-gray-900 truncate">Vue Hired MovieDB!</h1>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    theme: {
+      extend: {
+        animation: {
+          'spin': 'spin 12s linear infinite',
+        }
+      }
+    }
+  }
